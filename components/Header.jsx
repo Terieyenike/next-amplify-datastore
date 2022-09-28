7e23a87e6e1534576a1a45336909ea76b1603c34
teri eyenike <teyenike1@gmail.com>
2022-09-28 01:15:17 +0100

    Add: the form fields nav link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
+import Menu from '../pages/recipe/[id]';
 import Logo from './Logo';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className='border-b border-gray-100'>
       <div className='max-w-7xl mx-auto w-11/12 flex justify-between items-center py-4'>
         <Logo />
+        <Link href='/menu-form'>
+          <a>Menu Form</a>
+        </Link>
       </div>
     </header>
   );
